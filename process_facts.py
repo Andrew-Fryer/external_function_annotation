fd9b9f03695b5f0c017eaf90f503ef8f10635f02
Andrew Fryer <andrew.fryer@queensu.ca>
2023-04-20 15:14:55 -0400

    .

--- process_facts.py
+++ process_facts.py
@@ -1,12 +1,27 @@
 import sys
 
 
+def convert_orign_fn_name(fn_name):
+    ind = fn_name.find('~')
+    if ind < 0:
+        return fn_name
+    ind = fn_name.find('::')
+    assert(ind >= 0)
+    fn_name = fn_name[ind + 2:]
+
+    ind_opening = fn_name.find('{')
+    ind_closing = fn_name.find('}')
+    if ind_opening >= 0 and ind_closing > ind_opening:
+        assert(ind_opening > 2 and fn_name[ind_opening - 1] == ':' and fn_name[ind_opening - 2] == ':')
+        fn_name = fn_name[:ind_opening - 2] + fn_name[ind_closing + 1:]
+    return fn_name
+
 called_by_relation = {} # is called by relation
 # relation_domain = set()
 called_by_relation_range = set()
 
 buffer = ""
-for line in sys.stdin:
+for line in open('./all_calls.txt', 'r'): #sys.stdin:
     line = line.replace("\n", "")
     line = line.replace(" ", "")
     if len(line) == 0:
@@ -25,6 +40,8 @@ for line in sys.stdin:
     
     origin_fn_name = line[:ind]
     target_fn_name = line[ind + 2:-1]
+
+    origin_fn_name = convert_orign_fn_name(origin_fn_name)
     
     if target_fn_name not in called_by_relation:
         called_by_relation[target_fn_name] = set()
@@ -47,7 +64,7 @@ for external_fn in external_fns:
         if external_fn not in transitive_call_relation[internal_fn]:
             transitive_call_relation[internal_fn].add(external_fn)
             for fn in called_by_relation.get(internal_fn, []):
-                work.push(fn)
+                work.append(fn)
 
 # print(called_by_relation)
 # print(transitive_call_relation)
