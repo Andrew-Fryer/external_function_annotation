5def49fb1d8c3352bb878a9f22ad172d90e1480d
Andrew Fryer <andrew.fryer@queensu.ca>
2023-04-19 20:57:51 -0400

    works to run 'txl -s 4000M ./thir-flat.txt call_island.txl | python3 process_facts.py'

--- process_facts.py
+++ process_facts.py
@@ -1,23 +1,14 @@
 import sys
 
 
-input_str = '''0 : 36 ~ andrew_fuzz [c4b5] : : core : : bit_array : : {impl # 0} : : new - > std : : rc : : Rc : : < std : : cell : : RefCell < std : : vec : : Vec < u8 > > > : : new;
-0 : 36 ~ andrew_fuzz [c4b5] : : core : : bit_array : : {impl # 0} : : new - > std : : cell : : RefCell : : < std : : vec : : Vec < u8 > > : : new;
-0 : 36 ~ andrew_fuzz [c4b5] : : core : : bit_array : : {impl # 0} : : new - > std : : vec : : Vec : : < u8 > : : len;
-0 : 36 ~ andrew_fuzz [c4b5] : : core : : bit_array : : {impl # 0} : : new - > core : : panicking : : panic;
-0 : 36 ~ andrew_fuzz [c4b5] : : core : : bit_array : : {impl # 0} : : new - > std : : vec : : Vec : : < u8 > : : len;
-0 : 36 ~ andrew_fuzz [c4b5] : : core : : bit_array : : {impl # 0} : : new - > < std : : vec : : Vec < u8 > as std : : ops : : IndexMut < usize > > : : index_mut;
-0 : 36 ~ andrew_fuzz [c4b5] : : core : : bit_array : : {impl # 0} : : new - > < std : : vec : : Vec < u8 > as std : : ops : : IndexMut < usize > > : : index_mut;
-0 : 36 ~ andrew_fuzz [c4b5] : : core : : bit_array : : {impl # 0} : : new - > std : : vec : : Vec : : < u8 > : : len;'''
-
 relation = {}
 
 buffer = ""
-for line in input_str.split('\n'): #sys.stdin:
+for line in sys.stdin:
+    line = line.replace("\n", "")
+    line = line.replace(" ", "")
     if len(line) == 0:
         continue
-    
-    line = line.replace(" ", "")
     
     if line[-1] != ";":
         buffer += line
