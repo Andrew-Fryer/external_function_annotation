b5b9cafd654ea92ecddb55bac077a910a8647f06
Andrew Fryer <andrew.fryer@queensu.ca>
2023-04-25 11:05:55 -0400

    I think dynamic dispatch goes all the way through now!

--- process_facts.py
+++ process_facts.py
@@ -70,31 +70,37 @@ def possible_callers_for_callee(callee):
     yield 
 internal_fns = callers
 external_fns = OrderedSet()
-callee_to_caller = {}
+callee_to_possible_callers = {}
 for callee in all_callees:
     for possible_caller in possible_callers_for_callee(callee):
         if possible_caller in internal_fns:
-            if callee in callee_to_caller:
+            if callee in callee_to_possible_callers:
                 # with dynamic dispatch, we find ambiguities regularly
                 # print('found ambiguity between', callee_to_caller[callee], 'and', possible_caller)
-                callee_to_caller[callee].add(possible_caller)
-            callee_to_caller[callee] = OrderedSet(possible_caller)
-    if callee not in callee_to_caller:
+                callee_to_possible_callers[callee].add(possible_caller)
+            callee_to_possible_callers[callee] = OrderedSet([possible_caller])
+    if callee not in callee_to_possible_callers:
         external_fns.add(callee)
+    
+caller_name_to_callee_names = {}
+for callee in callee_to_possible_callers:
+    for caller in callee_to_possible_callers[callee]:
+        if caller not in caller_name_to_callee_names:
+            caller_name_to_callee_names[caller] = OrderedSet()
+        caller_name_to_callee_names[caller].add(callee)
 
 # Build the transitive relation from internal functions to external functions
 transitive_internal_calls_external = {}
 for fn in internal_fns:
     transitive_internal_calls_external[fn] = OrderedSet()
 for external_fn in external_fns:
-    work = list(called_by[external_fn])
+    work = list(called_by[external_fn]) # note that work contains caller names (as opposed to callee names)
     while len(work) > 0:
         internal_fn = work.pop()
-        if external_fn not in transitive_internal_calls_external[internal_fn]:
+        if external_fn not in transitive_internal_calls_external[internal_fn]: # this takes care of duplicate calls and direct and indirect recursion
             transitive_internal_calls_external[internal_fn].add(external_fn)
-            if internal_fn in callee_to_caller:
-                internal_fn = callee_to_caller.get(internal_fn)
-                for fn in called_by[internal_fn]:
+            for callee in caller_name_to_callee_names.get(internal_fn, []):
+                for fn in called_by[callee]:
                     work.append(fn)
 
 # Print out the results
